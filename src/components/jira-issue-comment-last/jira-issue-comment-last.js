4e7bed652a308b0428869154e3469ea76c2f15d6
dimokgit <dimokdimon@gmail.com>
2016-01-11 15:51:10 -0500

    Minor fixes: swaggerErrorParser,addComment

--- src/components/jira-issue-comment-last/jira-issue-comment-last.js
+++ src/components/jira-issue-comment-last/jira-issue-comment-last.js
@@ -7,12 +7,30 @@ define(['knockout', 'text!./jira-issue-comment-last.html', "objecter"], function
       return ((issueFields().comment || {}).comments || [])
         .slice(-1)
         .map(function (c) {
-          var d = new Date(c.created);
+          var d = parseJiraDate(c.created);
           return "["+c.author.displayName +"] on "+d.toDateString() + " " + d.toLocaleTimeString() + ":\n" + c.body;
         })[0];
     });
   }
   
+  function parseJiraDate(jiraDate) {
+    function isNotEmpty(s) { return s; }
+    var dateAndZone = jiraDate.split(/^(.+)([-+]\d+)$/).filter(isNotEmpty);
+    var date = Date.parse(dateAndZone[0]);
+    var zoneHourMinute = dateAndZone[1].split(/([-+]?\d\d)/).filter(isNotEmpty);
+    var zoneHours = parseInt(zoneHourMinute[0]);
+    var zoneMinutes =  zoneHours * 60 + parseInt(zoneHourMinute[1]);
+    var resDate = new Date(date - zoneMinutes * 60000);
+    return createDateAsUTC(resDate);
+  }
+
+  function createDateAsUTC(date) {
+    return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()));
+  }
+
+  function convertDateToUTC(date) {
+    return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
+  }
   return { viewModel: JiraIssueCommentLast, template: templateMarkup };
 
 });
